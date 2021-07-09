7d11ce468946622472b7a86a1b640abdd58a6bbc
abuu <shayxislomovabdurahmon.com>
2021-07-09 18:46:02 +0500

    enable prod devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  define: {
+    __VUE_PROD_DEVTOOLS__: true,
   },
 })
